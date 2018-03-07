d7d210d8d0a1a70978f2e334c83531b612d0482b
Shobhit.Sachdeva <Shobhit.Sachdeva@IPAD81145.synechron.com>
2018-03-07 12:40:10 +0530

    ClientEditor

---
+++ src/techgig/CandidateCode.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package techgig;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Shobhit.Sachdeva
+ */
+public class CandidateCode {
+    public static void main(String[] args) {
+        
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Input");
+        String s = sn.nextLine();
+        int i = sn.nextInt();
+        float f = sn.nextFloat();
+        System.out.println("Output");
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+    }
+
+}
